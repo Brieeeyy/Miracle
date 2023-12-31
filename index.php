ed4fa0aa62e7eb41132fcea9c1d4dfd3eabb1ed0
Brieeeyy <144513690+Brieeeyy@users.noreply.github.com>
2023-12-31 17:11:37 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,18 @@
-<?php
-require_once("../../include/initialize.php");
-//checkAdmin();
-	# code...
-if(!isset($_SESSION['USERID'])){
-	redirect(web_root."admin/index.php");
+<?php 
+require_once("../include/initialize.php");
+	 if (!isset($_SESSION['USERID'])){
+      redirect(web_root."admin/login.php");
+     } 
+
+$content='home.php';
+$view = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+switch ($view) {
+	case '1' :
+        $title="Products";	
+		$content='products/';		
+		break;	
+	default :		
+	redirect("products/");
 }
-
-$view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
-
-	$header=$view;
-	$title="Products"; 
-	switch ($view) {
-
-	case 'list' :
-	 
-		$content    = 'list.php';		
-		break;
-
-	case 'add' : 
-		$content    = 'add.php';		
-		break;
-
-	case 'edit' : 
-		$content    = 'edit.php';		
-		break;
-
-	case 'view' : 
-		$content    = 'view.php';
-		break;
-  
-
-  	default :
-	$title="Products";
-		$content    = 'list.php';
-	}
-
-
-   
- 
-require_once ("../theme/templates.php");
+require_once("theme/templates.php");
 ?>
-  
